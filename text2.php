4a01c6e6c7ccff217bcd2865655604bc1fbd758b
Roman <cuclovod5611@gmail.com>
2016-04-18 13:34:53 +0300

    Delete text2.php

--- text2.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "dfbkfekj";
-$arr = glob('Blogs/*.txt');
-foreach($arr as $key=>$value){
-    echo "Блог № $keyе".PHP_EOL;
-    echo file_get_contents($value).PHP_EOL;
-    echo "".PHP_EOL;
-}
